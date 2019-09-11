345f2bfbd92210bfe3d989cb601e81a63853a0c8
renakeji <renakeji@163.com>
2019-09-11 17:24:31 +0800

    first commit

---
+++ demo.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * @Author: renakeji
+ * @Date:   2019-09-11 16:58:11
+ * @Last Modified by:   renakeji
+ * @Last Modified time: 2019-09-11 16:59:10
+ */
+
+include('./vendor/autoload.php');
+
+use Renakeji\Phptools\Rstring;
+
+Rstring::hello();
---
+++ src/Renakeji/Phptools/Rstring.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * @Author: renakeji
+ * @Date:   2019-09-11 16:49:57
+ * @Last Modified by:   renakeji
+ * @Last Modified time: 2019-09-11 16:59:49
+ */
+namespace Renakeji\Phptools;
+
+/**
+ * summary
+ */
+class Rstring
+{
+    public static function hello()
+    {
+        echo "hello";
+    }
+}
