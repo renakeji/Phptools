2d922a01c68a98c686196d8c87a179b4edb00f23
renakeji <renakeji@163.com>
2019-09-11 17:50:12 +0800

    test2

--- src/Renakeji/Phptools/Rstring.php
+++ src/Renakeji/Phptools/Rstring.php
@@ -4,7 +4,7 @@
  * @Author: renakeji
  * @Date:   2019-09-11 16:49:57
  * @Last Modified by:   renakeji
- * @Last Modified time: 2019-09-11 16:59:49
+ * @Last Modified time: 2019-09-11 17:49:47
  */
 namespace Renakeji\Phptools;
 
@@ -16,5 +16,10 @@ class Rstring
     public static function hello()
     {
         echo "hello";
+    }
+
+    public static function toString()
+    {
+        echo 'toString';
     }
 }
